57f6d24800b9a57cb5b08911495d9a4c3086cc52
sailent <sailent@users.noreply.github.com>
2021-11-20 12:05:19 +0000

    Deploying to gh-pages from @ sailent/Skypro-FE-Course-2@2c41198c44e7b779e99e2a0b763775edb7d19351 🚀

--- Velko/index.js
+++ Velko/index.js
@@ -4,7 +4,6 @@ let price = '300';
 let product_price = `${product}, цена ${price} рублей`;
 // eslint-disable-next-line
 console.log(product_price);
-
 function randomize(min, max) {
   min = Math.ceil(min); max = Math.floor(max);
   if (min >= max)
@@ -15,4 +14,3 @@ let min = prompt('Введите min');
 let max = prompt('Введите max');
 // eslint-disable-next-line
 console.log(randomize(min, max));
-
